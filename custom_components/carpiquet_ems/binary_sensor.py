583a559bef509ff7d0ec1c03fb888f7c3750897e
morgannormand14-cell <morgan.normand.14@gmail.com>
2026-09-20 19:30:04 +0200

    Attach Write Gate diagnostics to master lock entity

--- custom_components/carpiquet_ems/binary_sensor.py
+++ custom_components/carpiquet_ems/binary_sensor.py
@@ -42,3 +42,15 @@ class CarpiquetBinarySensor(CoordinatorEntity, BinarySensorEntity):
     @property
     def is_on(self):
         return bool(self.coordinator.data.get(self._data_key, False))
+
+    @property
+    def extra_state_attributes(self):
+        if self._data_key == ATTR_WRITE_GATE_MASTER_LOCK:
+            return {
+                "gate_state": self.coordinator.data.get(ATTR_WRITE_GATE_STATE),
+                "execute_allowed": self.coordinator.data.get(ATTR_WRITE_GATE_EXECUTE_ALLOWED),
+                "blockers": self.coordinator.data.get(ATTR_WRITE_GATE_BLOCKERS),
+                "evaluated_at": self.coordinator.data.get(ATTR_WRITE_GATE_EVALUATED_AT),
+                "version": VERSION,
+            }
+        return {"version": VERSION}
